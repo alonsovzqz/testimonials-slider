53b18f4d9d18fdc901ab44bc45e9433debd2681f
Alonso Vazquez <haloanderson45@gmail.com>
2020-06-10 18:05:02 -0500

    Refactor the section to remove classes. Improve the event listener for the previous slider.

--- js/script.js
+++ js/script.js
@@ -3,16 +3,35 @@ const SLIDER_PREV = document.querySelector(".slider__control.control--prev");
 const SLIDER_NEXT = document.querySelector(".slider__control.control--next");
 let currentSlider = 0;
 let nextSlider = currentSlider + 1;
-let previousSlider = 0;
+let previousSlider =
+  currentSlider === 0 ? TESTIMONIALS.length - 1 : currentSlider - 1;
+
+function removeAnimationSliderClasses() {
+  setTimeout(function () {
+    TESTIMONIALS.forEach((slide) => {
+      if (slide.classList.contains("slider__item-next")) {
+        slide.classList.remove("slider__item-next");
+      } else if (slide.classList.contains("slider__item-prev")) {
+        slide.classList.remove("slider__item-prev");
+      }
+    });
+  }, 500);
+
+  clearTimeout();
+}
 
 SLIDER_PREV.addEventListener("click", function () {
+  TESTIMONIALS[currentSlider].classList.add("slider__item-next");
+  TESTIMONIALS[previousSlider].classList.add("slider__item-prev");
+
   TESTIMONIALS[currentSlider].classList.remove("item--active");
-  if (currentSlider === 0) {
-    currentSlider = TESTIMONIALS.length - 1;
-  } else {
-    currentSlider--;
-  }
-  TESTIMONIALS[currentSlider].classList.add("item--active");
+  TESTIMONIALS[previousSlider].classList.add("item--active");
+
+  currentSlider = previousSlider;
+  previousSlider =
+    currentSlider === 0 ? TESTIMONIALS.length - 1 : currentSlider - 1;
+
+  removeAnimationSliderClasses();
 });
 
 SLIDER_NEXT.addEventListener("click", function () {
@@ -25,15 +44,5 @@ SLIDER_NEXT.addEventListener("click", function () {
   currentSlider = nextSlider;
   nextSlider = nextSlider === TESTIMONIALS.length - 1 ? 0 : nextSlider + 1;
 
-  setTimeout(function () {
-    TESTIMONIALS.forEach((slide) => {
-      if (slide.classList.contains("slider__item-next")) {
-        slide.classList.remove("slider__item-next");
-      } else if (slide.classList.contains("slider__item-prev")) {
-        slide.classList.remove("slider__item-prev");
-      }
-    });
-  }, 500);
-
-  clearTimeout();
+  removeAnimationSliderClasses();
 });
