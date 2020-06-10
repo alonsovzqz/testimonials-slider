d1ed244cbd17b8a8ad06798d0468a766d10c6789
Alonso Vazquez <haloanderson45@gmail.com>
2020-06-10 01:48:10 -0500

    Remove unnecessary styles from the CSS file. Remove the unnecessary classes from the HTML. Improve the next slide event.

--- js/script.js
+++ js/script.js
@@ -2,43 +2,33 @@ const TESTIMONIALS = document.querySelectorAll(".slider__item");
 const SLIDER_PREV = document.querySelector(".slider__control.control--prev");
 const SLIDER_NEXT = document.querySelector(".slider__control.control--next");
 let currentSlider = 0;
+let nextSlider = currentSlider + 1;
+let previousSlider = 0;
 
-SLIDER_PREV.addEventListener("click", function (e) {
+SLIDER_PREV.addEventListener("click", function () {
   TESTIMONIALS[currentSlider].classList.remove("item--active");
-  TESTIMONIALS[currentSlider].classList.add("slider__item-next");
-  currentSlider--;
-  TESTIMONIALS[currentSlider].classList.add(
-    "item--active",
-    "slider__item-prev"
-  );
-  setTimeout(function () {
-    TESTIMONIALS[currentSlider].classList.remove("slider__item-prev");
-    TESTIMONIALS[currentSlider + 1].classList.remove("slider__item-next");
-  }, 50);
-  SLIDER_NEXT.classList.remove("disabled");
-
   if (currentSlider === 0) {
-    SLIDER_PREV.classList.add("disabled");
-    e.preventDefault();
+    currentSlider = TESTIMONIALS.length - 1;
+  } else {
+    currentSlider--;
   }
+  TESTIMONIALS[currentSlider].classList.add("item--active");
 });
 
-SLIDER_NEXT.addEventListener("click", function (e) {
-  TESTIMONIALS[currentSlider].classList.remove("item--active");
+SLIDER_NEXT.addEventListener("click", function () {
   TESTIMONIALS[currentSlider].classList.add("slider__item-prev");
-  currentSlider++;
-  TESTIMONIALS[currentSlider].classList.add(
-    "item--active",
-    "slider__item-next"
-  );
-  setTimeout(function () {
-    TESTIMONIALS[currentSlider].classList.remove("slider__item-next");
-    TESTIMONIALS[currentSlider - 1].classList.remove("slider__item-prev");
-  }, 50);
-  SLIDER_PREV.classList.remove("disabled");
+  TESTIMONIALS[nextSlider].classList.add("slider__item-next");
 
-  if (TESTIMONIALS.length === currentSlider + 1) {
-    SLIDER_NEXT.classList.add("disabled");
-    e.preventDefault();
-  }
+  TESTIMONIALS[currentSlider].classList.remove("item--active");
+  TESTIMONIALS[nextSlider].classList.add("item--active");
+
+  setTimeout(function () {
+    TESTIMONIALS[currentSlider].classList.remove("slider__item-prev");
+    TESTIMONIALS[nextSlider].classList.remove("slider__item-next");
+  }, 500);
+
+  clearTimeout();
+
+  currentSlider = nextSlider;
+  nextSlider = nextSlider === TESTIMONIALS.length - 1 ? 0 : nextSlider + 1;
 });
