6752e265efb619d6c89075947668b9204efcfef9
Alonso Vazquez <haloanderson45@gmail.com>
2020-06-28 03:28:21 -0500

    Make the proper fixes for the responsiveness. Add new animations. Change the script for animations.

--- js/script.js
+++ js/script.js
@@ -20,20 +20,24 @@ function getCurrentSlider() {
 function removeAnimationSliderClasses() {
   setTimeout(function () {
     TESTIMONIALS.forEach((slide) => {
-      if (slide.classList.contains("slider__item-next")) {
-        slide.classList.remove("slider__item-next");
-      } else if (slide.classList.contains("slider__item-prev")) {
-        slide.classList.remove("slider__item-prev");
+      if (slide.classList.contains("backOutRight")) {
+        slide.classList.remove("backOutRight");
+      } else if (slide.classList.contains("backOutLeft")) {
+        slide.classList.remove("backOutLeft");
+      } else if (slide.classList.contains("backInLeft")) {
+        slide.classList.remove("backInLeft");
+      } else if (slide.classList.contains("backInRight")) {
+        slide.classList.remove("backInRight");
       }
     });
-  }, 500);
+  }, 1000);
 
   clearTimeout();
 }
 
 SLIDER_PREV.addEventListener("click", function () {
-  TESTIMONIALS[currentSlider].classList.add("slider__item-next");
-  TESTIMONIALS[previousSlider].classList.add("slider__item-prev");
+  TESTIMONIALS[currentSlider].classList.add("backOutRight");
+  TESTIMONIALS[previousSlider].classList.add("backInLeft");
 
   TESTIMONIALS[currentSlider].classList.remove("item--active");
   TESTIMONIALS[previousSlider].classList.add("item--active");
@@ -44,8 +48,8 @@ SLIDER_PREV.addEventListener("click", function () {
 });
 
 SLIDER_NEXT.addEventListener("click", function () {
-  TESTIMONIALS[currentSlider].classList.add("slider__item-prev");
-  TESTIMONIALS[nextSlider].classList.add("slider__item-next");
+  TESTIMONIALS[currentSlider].classList.add("backOutLeft");
+  TESTIMONIALS[nextSlider].classList.add("backInRight");
 
   TESTIMONIALS[currentSlider].classList.remove("item--active");
   TESTIMONIALS[nextSlider].classList.add("item--active");
